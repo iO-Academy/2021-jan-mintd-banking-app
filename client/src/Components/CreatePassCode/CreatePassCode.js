6e8d3c632435272957c07eb0e51b3b00d9df1041
Damien Scarpellini <damienscarpellini@yahoo.co.uk>
2021-04-27 16:32:27 +0100

    changed hardcoded type into a variable in textinputbox created and rendered passcode page with some validation

--- client/src/Components/CreatePassCode/CreatePassCode.js
+++ client/src/Components/CreatePassCode/CreatePassCode.js
@@ -10,13 +10,39 @@ class CreatePassCode extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            passCode: ''
+            firstPassCode: '',
+            secondPassCode: '',
+            passCode: '',
+            btnText: 'Enter Valid Passcode',
+            link: '/create-pass-code'
         }
     }
 
-    validatePasscode(){
+    firstPassCodeInput = (e) => {
+        let firstPassCode = e.target.value
+        this.setState ({
+            firstPassCode: firstPassCode
+            }
+        )}
 
+    secondPassCodeInput = (e) => {
+        let secondPassCode = e.target.value
+        this.setState ({
+            secondPassCode: secondPassCode
+            }
+        )}
+
+    // s2t3 woops
+    validatePassCode(){
+        let first = this.state.firstPassCode;
+        let second = this.state.secondPassCode;
+        first === second ? this.setState({
+            passcode: first,
+            btnText: 'Register',
+            link: '/main-accounts'
+        }) : alert('Passcodes do not match');
     }
+
 
     render(){
         return(
@@ -25,12 +51,12 @@ class CreatePassCode extends React.Component {
 
                 <Title titleText={"Create Your Passcode"} />
                 <div className={'auth-inputs-container'}>
-                    <TextInputBox change={(e) => this.nameInput(e)} placeholder={'create 6 digit passcode'} />
-                    <TextInputBox change={(e) => this.customerNumberInput(e)} placeholder={'re-enter 6 digit passcode'} />
+                    <TextInputBox type={"password"} change={(e) => this.firstPassCodeInput(e)} placeholder={'create 6 digit passcode'} />
+                    <TextInputBox type={"password"} change={(e) => this.secondPassCodeInput(e)} placeholder={'re-enter 6 digit passcode'} />
                 </div>
                 <UserContext.Provider value={{isRegistered: this.state.isRegistered, username: this.state.username, customerNumber: this.state.customerNumber}}>
-                    <Link to={'/main-accounts'}>
-                        <ActionButton btnText={"register"}></ActionButton>
+                    <Link to={this.state.link}>
+                        <ActionButton click={() => this.validatePassCode()} btnText={this.state.btnText}></ActionButton>
                     </Link>
                 </UserContext.Provider>
             </div>
--- client/src/Components/Registration/Registration.js
+++ client/src/Components/Registration/Registration.js
@@ -38,8 +38,8 @@ class Registration extends React.Component{
 
                 <Title titleText={"Registration"} />
                 <div className={'auth-inputs-container'}>
-                    <TextInputBox change={(e) => this.nameInput(e)} placeholder={'name'} />
-                    <TextInputBox change={(e) => this.customerNumberInput(e)} placeholder={'1234-5678-9999'} />
+                    <TextInputBox type={'text'} change={(e) => this.nameInput(e)} placeholder={'name'} />
+                    <TextInputBox type={'text'} change={(e) => this.customerNumberInput(e)} placeholder={'1234-5678-9999'} />
                 </div>
                 <UserContext.Provider value={{isRegistered: this.state.isRegistered, username: this.state.username, customerNumber: this.state.customerNumber}}>
                     <Link to={'/create-pass-code'}>
